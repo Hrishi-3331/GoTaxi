77d7a5f9a256fd1338ac34a95f1c6133db5e51f5
Hrishi-3331 <hrishikeshugle3331@gmail.com>
2020-04-14 13:34:42 +0530

    Main Screen UI done

--- app/src/main/java/com/hrishi3331studio/gotaxi/General/MainActivity.java
+++ app/src/main/java/com/hrishi3331studio/gotaxi/General/MainActivity.java
@@ -1,8 +1,11 @@
 package com.hrishi3331studio.gotaxi.General;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.net.ConnectivityManager;
+import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -29,9 +32,29 @@ import com.hrishi3331studio.gotaxi.Support.Support;
 import com.hrishi3331studio.gotaxi.User.Profile;
 import com.hrishi3331studio.gotaxi.UserAuth.SignUp;
 import com.makeramen.roundedimageview.RoundedImageView;
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineListener;
+import com.mapbox.android.core.location.LocationEnginePriority;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
+import com.mapbox.services.android.navigation.v5.milestone.Milestone;
+import com.mapbox.services.android.navigation.v5.milestone.MilestoneEventListener;
+import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress;
 import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationEngineListener, PermissionsListener, MapboxMap.OnMapClickListener{
 
     private DrawerLayout mDrawer;
     private NavigationView mNavigation;
@@ -40,10 +63,31 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private LoaderDialog dialog;
 
+    private MapView mapView;
+    private LocationLayerPlugin locationLayerPlugin;
+    private LocationEngine engine;
+    private PermissionsManager permissionsManager;
+    private MapboxMap mapboxMap;
+    private Location origin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Mapbox.getInstance(MainActivity.this, getString(R.string.access_token));
+
+        mapView = (MapView)findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(this);
+
+        MilestoneEventListener listener = new MilestoneEventListener() {
+            @Override
+            public void onMilestoneEvent(RouteProgress routeProgress, String instruction, Milestone milestone) {
+
+            }
+        };
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -172,19 +216,9 @@ public class MainActivity extends AppCompatActivity {
 
         final android.app.AlertDialog exit_dialog = builder.create();
 
-        ok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        ok.setOnClickListener(v -> finish());
 
-        cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                exit_dialog.dismiss();
-            }
-        });
+        cancel.setOnClickListener(v -> exit_dialog.dismiss());
 
         exit_dialog.show();
     }
@@ -209,5 +243,122 @@ public class MainActivity extends AppCompatActivity {
 
         getMenuInflater().inflate(R.menu.menu_event_details, menu);
         return true;
+    }
+
+    @SuppressLint("MissingPermission")
+    public void startLocation(){
+        if (PermissionsManager.areLocationPermissionsGranted(MainActivity.this)){
+            engine = new LocationEngineProvider(MainActivity.this).obtainBestLocationEngineAvailable();
+            engine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
+            engine.activate();
+
+            Location last_location = engine.getLastLocation();
+            if (last_location != null){
+                origin = last_location;
+                setCameraPosition(origin);
+            }else {
+                engine.addLocationEngineListener(this);
+            }
+
+            locationLayerPlugin = new LocationLayerPlugin(mapView, mapboxMap, engine);
+            locationLayerPlugin.setLocationLayerEnabled(true);
+            locationLayerPlugin.setCameraMode(CameraMode.TRACKING);
+            locationLayerPlugin.setRenderMode(RenderMode.NORMAL);
+
+        }
+        else {
+            permissionsManager = new PermissionsManager(MainActivity.this);
+            permissionsManager.requestLocationPermissions(MainActivity.this);
+        }
+    }
+
+    public void setCameraPosition(Location location){
+
+        mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(),16));
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onConnected() {
+        engine.requestLocationUpdates();
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            origin = location;
+            setCameraPosition(location);
+        }
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted){
+            startLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+        if (engine != null){
+            engine.deactivate();
+        }
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng point) {
+
+    }
+
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        this.mapboxMap = mapboxMap;
+        mapboxMap.setOnMapClickListener(this);
+
+
     }
 }
